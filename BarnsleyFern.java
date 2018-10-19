23b61cadf4a654a3757139ef09b3fa67b90d3ea6
Nigel AOYANG <nigel.aoyang@tap-group.com.cn>
2018-10-19 10:58:20 +0800

    Added algorithms

--- BarnsleyFern.java
+++ BarnsleyFern.java
@@ -2,22 +2,31 @@ import java.awt.BasicStroke;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JPanel;
 
 public class BarnsleyFern extends JPanel{
 	private static final long serialVersionUID = 8320182863233274022L;
 	
-	private Point p;
+	private List<Point> points;
 
 	public BarnsleyFern(int pos_x, int pos_y) {
-		p = new Point(pos_x, pos_y);
+		points = new ArrayList<Point>();
+		addPoint(new Point(pos_x, pos_y));
+	}
+	
+	public void addPoint(Point p) {
+		points.add(p);
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		Graphics2D g2D = (Graphics2D)g;
 		g2D.setStroke(new BasicStroke(10));
-		g2D.drawLine(p.x, p.y, p.x, p.y);
+		for (Point p : points) {
+			g2D.drawLine(p.x, p.y, p.x, p.y);
+		}
 	}
 }
--- MainFrame.java
+++ MainFrame.java
@@ -1,5 +1,9 @@
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Point;
 import java.awt.Toolkit;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
@@ -7,7 +11,11 @@ import javax.swing.WindowConstants;
 public class MainFrame extends JFrame{
 	private static final long serialVersionUID = -1616367561107980353L;
 
-	BarnsleyFern fern;
+	private BarnsleyFern fern;
+	private int centerx;
+	private int centery;
+	private int currentx;
+	private int currenty;
 
 	public static void main(String[] args){
 		try{
@@ -20,9 +28,11 @@ public class MainFrame extends JFrame{
 	}
 
 
-	public MainFrame(){
+	public MainFrame() throws InterruptedException{
 		setTitle("BarnsleyFern");
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//		getContentPane().setBackground(Color.white);
+		
 		Toolkit kit = Toolkit.getDefaultToolkit();
 		Dimension screen = kit.getScreenSize();
 		
@@ -33,18 +43,64 @@ public class MainFrame extends JFrame{
 		
 		setBounds(posx, posy, width, height);
 		
-		fern = new BarnsleyFern(width / 2, height / 2);
-		add(fern);
+//		centerx = width/2;
+//		centery = height/2;
+		centerx= 0;
+		centery = 0;
 		
+		fern = new BarnsleyFern(centerx, centery);
+		add(fern);
 	}
 	
 	
 	private void display() throws InterruptedException {
 		setVisible(true);
-
-//		while(true) {
-//			fern.repaint();
-//			Thread.sleep(10);
-//		}
+		
+		currentx = centerx;
+		currenty = centery;
+		
+		Timer timer = new Timer(true);
+		timer.schedule(new TimerTask() {
+			
+			@Override
+			public void run() {
+				double rand = Math.random();
+				System.out.println("Rand: " + rand + ", X: " + currentx + ", Y: " + currenty);
+				
+				int toApply = 0;
+				if (rand == 0.99) toApply = 1;
+				else if (rand >= 0.92) toApply = 2;
+				else if (rand >= 0.85) toApply = 3;
+				else toApply = 4;
+				
+				updatePosition(currentx, currenty, toApply);
+				fern.addPoint(new Point(currentx, currenty));
+				fern.repaint();
+			}
+		}, 100, 100);
+	}
+	
+	private void updatePosition(int x, int y, int algo) {
+		switch(algo) {
+		case 1:
+			currentx = centerx;
+			currenty = (int)(0.16 * y);
+			break;
+		case 2:
+			currentx = (int)((-0.15 * x) + (0.28 * y));
+			currenty = (int)((0.26 * x) + (0.24 * y) + 1);
+			break;
+		case 3:
+			currentx = (int)((0.2 * x) - (0.26 * y));
+			currenty = (int)((0.23 * x) + (0.22 * y) + 2);
+			break;
+		case 4:
+			currentx = (int)((0.85 * x) + (0.04 * y));
+			currenty = (int)((-0.04 * x) + (0.85 * y) + 2);
+			break;
+		default:
+			System.out.println("No algo");
+				
+		}
 	}
 }
